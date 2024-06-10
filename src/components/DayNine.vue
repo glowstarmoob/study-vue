ec398f98e5a2a5c38b66dbda9b8f8d7e635c62d6
starrymoonlight <94371604+glowstarmoob@users.noreply.github.com>
2024-06-10 21:12:07 +0800

    推送事件处理知识学习笔记和代码

--- src/components/DayNine.vue
+++ src/components/DayNine.vue
@@ -1,6 +1,70 @@
 <template>
   <div>
-    <p>This is DayNine.</p>
+    <p>This is DayNine.事件处理--监听事件、事件修饰符</p>
+<!--    语法:
+    &lt;!&ndash;完整写法&ndash;&gt;
+    <button v-on:事件名="函数名/vue表达式">点我</button>
+    &lt;!&ndash;简化写法&ndash;&gt;
+    <button @事件名="函数名/vue表达式">点我</button>-->
+    <button @click="count += 1">Add 1</button>
+    <p>The button above has been clicked {{ count }} times.</p>
+
+    <!-- `greet` 是在下面定义的方法名 -->
+    <button v-on:click="greet">Greet</button>
+    <p>{{counter}}</p>
+
+<!--    内联处理器中的方法-->
+    <button v-on:click="say('hi', $event)">Say hi</button>
+    <button v-on:click="say('what', $event)">Say what</button>
+
+<!--    在事件处理程序中调用 event.preventDefault() 或 event.stopPropagation() 是非常常见的需求。
+    尽管我们可以在方法中轻松实现这点，但更好的方式是：方法只有纯粹的数据逻辑，而不是去处理 DOM 事件细节。
+    为了解决这个问题，Vue.js 为 v-on 提供了事件修饰符。之前提过，修饰符是由点开头的指令后缀来表示的。
+
+    语法：	<button @事件名.事件修饰符="函数名/vue表达式"></button>
+    prevent：阻止默认事件（常用）
+    stop：阻止事件冒泡（常用）
+    once：事件只触发一次（常用）
+    capture：使用事件的捕获模式
+    self：只有event.target是当前操作的元素时才触发事件
+    passive：事件的默认行为立即执行，无需等待事件回调执行完毕（.passive 修饰符尤其能够提升移动端的性能。）
+    修饰符可以连续写，比如可以这么用：@click.prevent.stop="showInfo"-->
+
+    <br>
+    <br>
+    <!-- 阻止默认事件 -->
+    <a href="http://www.bing.com" @click.prevent="showInfo">点击默认事件</a>
+
+    <!-- 阻止事件冒泡 -->
+    <div class="demo1" @click="showInfo">
+      <button @click.stop="showInfo">点击事件冒泡</button>
+    </div>
+
+    <!-- 事件只触发一次 -->
+    <button @click.once="showInfo">点击只触发一次</button>
+
+    <!-- 使用事件的捕获模式 -->
+    <div class="box1" @click.capture="showMsg(1)">
+      div1
+      <div class="box2" @click="showMsg(2)">
+        div2
+      </div>
+    </div>
+
+    <!-- 只有event.target是当前操作的元素时才触发事件 -->
+    <div class="demo1" @click.self="showInfo">
+      <button @click="showInfo">点我只有event.target是当前操作的元素时才触发事件</button>
+    </div>
+
+    <!-- 事件的默认行为立即执行，无需等待事件回调执行完毕 -->
+    <!-- passive的作用 让事件的默认行为立即执行，无需等待事件回调执行完毕
+         @wheel.passive  让鼠标滚轮滚动的时候先触发默认行为再执行事件回调 -->
+    <ul @wheel.passive="demo" class="list">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
 
   </div>
 </template>
@@ -10,12 +74,35 @@ export default {
   name: 'DayNine',
   data () {
     return {
-
+      count: 0,
+      counter: 0,
+      name:'starry'
 
     }
   },
   methods:{
-
+    greet: function () {
+      this.counter += 1
+    },
+    say: function (message, event) {
+      // 现在我们可以访问原生事件对象
+      if (event) {
+        event.preventDefault()
+      }
+      alert(message)
+    },
+    showInfo(){
+      alert('你好！')
+    },
+    showMsg(msg){
+      console.log(msg)
+    },
+    demo(){
+      for (let i = 0; i < 10000; i++) {
+        console.log('#')
+      }
+      console.log('太多了，累坏了')
+    }
   }
 };
 
@@ -23,5 +110,26 @@ export default {
 
 <style scoped>
 /* 添加页面样式 */
+.demo1{
+  height: 50px;
+  background-color: skyblue;
+}
+.box1{
+  padding: 5px;
+  background-color: skyblue;
+}
+.box2{
+  padding: 5px;
+  background-color: orange;
+}
+.list{
+  width: 200px;
+  height: 200px;
+  background-color: peru;
+  overflow: auto;
+}
+li{
+  height: 100px;
+}
 </style>
 
--- src/components/DayTen.vue
+++ src/components/DayTen.vue
@@ -1,6 +1,54 @@
 <template>
   <div>
-    <p>This is DayTen.</p>
+    <p>This is DayTen.事件处理--按键修饰符、系统修饰符</p>
+    <!-- 只有在 `key` 是 `Enter` 时调用 `vm.submit()` -->
+    <input v-on:keyup.enter="submit">
+    <!-- PageDown 时被调用 -->
+    <!-- event.key 获取发生按键事件时按下的键盘按钮 -->
+    <input v-on:keyup.page-down="onPageDown">
+
+<!--    为了在必要的情况下支持旧浏览器，Vue 提供了绝大多数常用的按键码的别名：
+    .enter
+    .tab
+    .delete (捕获“删除”和“退格”键)
+    .esc
+    .space
+    .up
+    .down
+    .left
+    .right-->
+    <!-- 只有在 `key` 是 `Enter` 时调用 `vm.submit()` -->
+    <!-- <input v-on:keyup.enter="submit"> -->
+    <!-- enter的键盘码 13 -->
+    <!-- <input v-on:keyup.13="submit"> -->
+    <!-- 自定义按键修饰符别名 customEnter -->
+    <input v-on:keyup.13="submit">
+
+    <!-- PageDown 时被调用 -->
+    <!-- event.key 获取发生按键事件时按下的键盘按钮 -->
+    <input v-on:keyup.page-down="onPageDown">
+
+    <!-- Alt + C -->
+    <input v-on:keyup.alt.67="clear">
+    <!-- Ctrl + Click -->
+    <div v-on:click.ctrl="doSomething">Do something</div>
+
+    <!-- 请注意修饰键与常规按键不同，在和 keyup 事件一起用时，事件触发时修饰键必须处于按下状态。
+    换句话说，只有在按住 ctrl 的情况下释放其它按键，才能触发 keyup.ctrl。而单单释放 ctrl 也不会触发事件。
+    如果你想要这样的行为，请为 ctrl 换用 keyCode：keyup.17。 -->
+
+    <!-- 即使 Alt 或 Shift 被一同按下时也会触发 -->
+    <button v-on:click.ctrl="onClick">A</button>
+
+    <!-- 有且只有 Ctrl 被按下的时候才触发 -->
+    <button v-on:click.ctrl.exact="onCtrlClick">A</button>
+
+    <!-- 没有任何系统修饰符被按下的时候才触发 -->
+    <button v-on:click.exact="onClick">A</button>
+
+    <!-- 点击鼠标右键 -->
+    <div v-on:click.left="doSomething1">Do something</div>
+
 
   </div>
 </template>
@@ -15,7 +63,27 @@ export default {
     }
   },
   methods:{
-
+    submit(){
+      alert('提交了！！！')
+    },
+    onPageDown(){
+      alert('点击了page-down')
+    },
+    clear(){
+      alert('Alt + C 触发了哦！！')
+    },
+    doSomething(){
+      alert('Ctrl + Click 触发了哦！！')
+    },
+    doSomething1(){
+      alert('点击鼠标左键 触发了哦！！')
+    },
+    onClick(){
+      alert('onClick')
+    },
+    onCtrlClick(){
+      alert('onCtrlClick')
+    }
   }
 };
 
